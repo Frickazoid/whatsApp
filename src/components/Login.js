2cd80ea8d14fd98a376412a8b8e1049989bd404f
Andrey <veden91@gmail.com>
2023-01-23 20:35:38 +0200

    Add some logic to "Submit" bnt

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,19 +8,44 @@ function Login() {
 
     function onSignIn({username, type}){
         const capsName = username.toUpperCase();
-        if (type = `Sing in`){
-        fetch(`http://localhost:8080/api/user/${capsName}`)
-            .then( response => response.json() )
-            .then( response => {
-                console.log(response.data)
-                if (response.data.length === 0){
-                    alert(`User ${username} not found`)
-                }else{
-                    nv(`/`);
-                }
-            } );
-        }else{
-            //registraction fetch
+        if (type === `Sing in`) {
+            console.log(`sign in path`)
+            fetch(`http://localhost:8080/api/user/${capsName}`)
+                .then(response => response.json())
+                .then(response => {
+                    console.log(response.data)
+                    if (response.data.length === 0) {
+                        alert(`User ${username} not found`)
+                    } else {
+                        nv(`/`)
+                    }
+                });
+        } else {
+            console.log(`registration path`)
+            const params = {
+                username: username,
+                capsName: capsName
+            };
+            const options = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(params)
+            };
+            console.log(params)
+            fetch( 'http://localhost:8080/api/createuser', options )
+                .then( response => {
+                    if (response.status === 400) {
+                        response.json()
+                        alert(response.statusText)
+                    } else {
+                        alert(`User ${username} create successfully`)
+                        //nv(`/`)
+                    }
+                })
+                .catch(error => {
+                    this.setState({ errorMessage: error.toString() });
+                    console.error('There was an error!', error);
+                });;
         }
     }
 
@@ -30,7 +55,7 @@ function Login() {
                 <SignInRegistraction isRegistration={false} onSignIn={onSignIn}/>
             </div>
             <div label="Registration">
-                <SignInRegistraction isRegistration={true}/>
+                <SignInRegistraction isRegistration={true} onSignIn={onSignIn}/>
             </div>
         </Tabs>
     )
